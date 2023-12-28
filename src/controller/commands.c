61c00d1970b4fee3f5834043f49233c02d7d861c
Iaio99 <glnvlln0@gmail.com>
2023-12-28 08:34:36 +0100

    Creata la funzione get_output

---
+++ src/controller/commands.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "commands.h"
+
+void get_output (char *command)
+{
+	int ret;
+	FILE *stream;
+	char output[4096] = {0};
+
+	stream = popen(command, "r");
+
+	if (stream == NULL)
+		exit(-1);
+
+	while ((ret = fread(output, sizeof(char), 4096, stream)) > 0) {
+		printf("%s\n", output);
+	}
+
+	pclose(stream);
+}
