460358d31a9d529e0c72bba39fcdd6beb0798b2d
Iaio99 <glnvlln0@gmail.com>
2023-12-30 01:20:06 +0100

    Aggiunto il banner e refactoring

--- src/view/menu.c
+++ src/view/menu.c
@@ -5,19 +5,25 @@
 #include "../controller/network.h"
 #include "../controller/disks.h"
 #include "../controller/cpu.h"
+#include "../utils/io.h"
 
 void menu()
 {
 	int opt;
 	
-	printf("Welcome in Troubleshooter!\n");
-	printf("Quale programma vuoi usare?\n");
+colorized_puts(BLUE, "__          __  _                            _         _______              _     _           _                 _            _ ");
+colorized_puts(BLUE, "\\ \\        / / | |                          (_)       |__   __|            | |   | |         | |               | |          | |");
+colorized_puts(BLUE, " \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___   _ _ __      | |_ __ ___  _   _| |__ | | ___  ___| |__   ___   ___ | |_ ___ _ __| |");
+colorized_puts(BLUE, "  \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | | '_ \\     | | '__/ _ \\| | | | '_ \\| |/ _ \\/ __| '_ \\ / _ \\ / _ \\| __/ _ \\ '__| |");
+colorized_puts(BLUE, "   \\  /\\  /  __/ | (_| (_) | | | | | |  __/ | | | | |    | | | | (_) | |_| | |_) | |  __/\\__ \\ | | | (_) | (_) | ||  __/ |  |_|");
+colorized_puts(BLUE, "    \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___| |_|_| |_|    |_|_|  \\___/ \\__,_|_.__/|_|\\___||___/_| |_|\\___/ \\___/ \\__\\___|_|  (_)");
+	colorized_puts(BLUE, "Quale programma vuoi usare?");
 	
 	while(1) {
-		printf("1. Network info\n");
-		printf("2. Test Connection\n");
-		printf("3. Disks info\n");
-		printf("4. CPU info\n");
+		colorized_puts(BLUE, "1. Network info");
+		colorized_puts(BLUE, "2. Test Connection");
+		colorized_puts(BLUE, "3. Disks info");
+		colorized_puts(BLUE, "4. CPU info");
 		
 		scanf("%d", &opt);
 		getchar();
@@ -36,7 +42,7 @@ void menu()
 				get_cpu_info();
 				break;
 			default:
-				printf("Unrecognized option\n");
+				print_error("Unrecognized option");
 				break;
 		}
 	}
